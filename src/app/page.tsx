29ab7be7bdf240d112ef05bb4070eeb559d30e60
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2026-05-24 21:05:14 +0000

    feat:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center p-8">
-      <h1 className="text-4xl font-semibold tracking-tight">we are running</h1>
+      <h1 className="text-4xl font-semibold tracking-tight">
+        we are running deployment number 2
+      </h1>
       <h2>
         {process.env.NAME}
         {process.env.DB_URL}
