35d26e6fa5f0c518fab1e9b8e0f8a68ba3264a06
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2026-05-24 20:13:21 +0000

    feat:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center p-8">
       <h1 className="text-4xl font-semibold tracking-tight">we are running</h1>
+      <h2>
+        {process.env.NAME}
+        {process.env.DB_URL}
+      </h2>
     </main>
   );
 }
